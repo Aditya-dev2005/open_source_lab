45ac832fe6e60c78437f6e0129e4beb1e95e77ee
Aditya-dev2005 <adityachat.5760@gmail.com>
2025-07-30 10:35:25 +0530

    Add files via upload

---
+++ str_rev.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string original;
+    cout<<"Enter the string : "<<endl;
+    cin>>original;
+    reverse(original.begin(), original.end());
+    cout<<"Reversed string is : "<<original<<endl;
+}
